f277b233a48c1018a6bb69373776649c8cf681ae
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-05-28 20:06:12 -0400

    chore(precommit-hook): add ESLint and Prettier pre-commit hook

--- src/data-structures/hello-world/linked-list.test.ts
+++ src/data-structures/hello-world/linked-list.test.ts
@@ -3,6 +3,7 @@ import HelloWorld from './index'
 describe('hello world test', () => {
   it('works', () => {
     const helloWorld = new HelloWorld()
+
     expect(helloWorld).toBeTruthy()
   })
 })
