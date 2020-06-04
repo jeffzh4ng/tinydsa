bf40f3b990f72a6a3ff16f3c4cb9b5834e8188bc
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-06-04 13:26:45 -0400

    feat(circular-buffer): create circular buffer

--- src/data-structures/index.ts
+++ src/data-structures/index.ts
@@ -1,6 +1,7 @@
 import LinkedList from './sequences/linked-list'
 import Stack from './sequences/stack'
 import { Queue, Deque } from './sequences/queue'
+import CircularBuffer from './sequences/circular-buffer'
 import { MinBinaryHeap, MinDHeap, MinIndexedDHeap } from './priority-queues'
 
 const Collections = {
@@ -8,6 +9,7 @@ const Collections = {
   Stack,
   Queue,
   Deque,
+  CircularBuffer,
   MinBinaryHeap,
   MinDHeap,
   MinIndexedDHeap,
