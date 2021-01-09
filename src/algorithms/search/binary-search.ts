6f1eeb3429d7eb70d7ec9b6cef8a2b9cbbf7d88b
ptran <phuoctran27@gmail.com>
2021-01-08 20:50:59 -0800

    Add recursive binary search

--- src/algorithms/search/binary-search.ts
+++ src/algorithms/search/binary-search.ts
@@ -1,20 +1,28 @@
+export type Item = number | string
+
 /**
- * Binary search an array of sorted numbers for a target value
- * Time complexity: O(log(n))
+ * Iteratively search an array of sorted items for a target item and returns its index.
+ *
+ * Time complexity:
+ *    - for an array of numbers: O(log(n)),
+ *    - for an array of strings: O(m*log(n)),
+ *    - where n is the number of array elements and m is the average string length.
+ *
  * Space complexity: O(1)
- * @param nums - array of sorted numbers to be searched
- * @param target - number to be searched for
- * @return index - position of target in nums array (or -1 if target is not found)
+ *
+ * @param arr - Sorted array of numbers or strings to be searched
+ * @param target - Number or string to be searched for
+ * @return index of target in array (or -1 if target is not found)
  */
-const binarySearch = (nums: number[], target: number): number => {
+export const binarySearchIterative = (arr: Item[], target: Item): number => {
   let lo = 0
-  let hi = nums.length - 1
+  let hi = arr.length - 1
   let mid
   while (lo <= hi) {
     mid = Math.floor((lo + hi) / 2)
-    if (nums[mid] > target) {
+    if (arr[mid] > target) {
       hi = mid - 1
-    } else if (nums[mid] < target) {
+    } else if (arr[mid] < target) {
       lo = mid + 1
     } else {
       return mid
@@ -23,4 +31,32 @@ const binarySearch = (nums: number[], target: number): number => {
   return -1
 }
 
-export default binarySearch
+/**
+ * Recursively search an array of sorted items for a target item and returns its index.
+ *
+ * Time complexity:
+ *    - for an array of numbers: O(log(n)),
+ *    - for an array of strings: O(m*log(n)),
+ *    - where n is the number of array elements and m is the average string length.
+ *
+ * Space complexity: O(log(n))
+ *
+ * @param arr - Sorted array of numbers or strings to be searched
+ * @param target - Number or string to be searched for
+ * @return index of target in array (or -1 if target is not found)
+ */
+export const binarySearchRecursive = (arr: Item[], target: Item): number => {
+  const search = (arr: Item[], target: Item, lo: number, hi: number): number => {
+    if (lo > hi) return -1
+    const mid = Math.floor((lo + hi) / 2)
+    if (arr[mid] > target) {
+      return search(arr, target, lo, mid - 1)
+    } else if (arr[mid] < target) {
+      return search(arr, target, mid + 1, hi)
+    } else {
+      return mid
+    }
+  }
+
+  return search(arr, target, 0, arr.length - 1)
+}
